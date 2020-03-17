868b6289bf0c9a7d373a79667f3be5a5d15fa809
tyler martin <t.mar92@gmail.com>
2020-03-17 11:19:33 -0300

    removes irrelevant comments, and reference to old component

--- src/app.js
+++ src/app.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<p>this is my boilerplate</p>, document.getElementById('app'));
-
-
-/*** USING props.children...
-const Layout = (props) => {
-  return (
-    <div>
-      <p>header</p>
-      {props.children}
-      <p>footer</p>
-    </div>
-  );
-};
-
-ReactDOM.render((
-  <Layout>
-    <div>
-      <h1>Page Title</h1>
-      <p>This is my page</p>
-    </div>
-  </Layout>
-), document.getElementById('app'));
-
-...props.children END***/
