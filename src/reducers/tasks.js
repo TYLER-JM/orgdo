01034db0d01bafa3084098ddea3be3988e5bc3f3
Tyler Martin <tyler.martin@electric-playground.com>
2022-06-18 15:01:55 -0300

    feature/normalized-state updates reducers to handle editing for normalized state

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -29,24 +29,26 @@ let normalizedState = {
 	allIds: ['task1', 'task2', 'task1a']
 }
 
+function editTask(state, action) {
+  return {
+    byId: state.byId.map(task => {
+      if (task.id === action.id) {
+        return {...task, ...action.updates}
+      }
+      return task
+    }),
+    allIds: [state.allIds]
+  }
+}
+
 export default (state = normalizedState, action) => {
   switch (action.type) {
     case 'ADD_TASK':
-      // return [...state, action.task]
       return {byId: [...state.byId, action.task], allIds: [...state.allIds, action.task.id]}
     case 'REMOVE_TASK':
-      return state.filter(task => task.id !== action.id);
+      return {byId: state.byId.filter(task => task.id !== action.id), allIds: state.allIds.filter(id => id !== action.id)};
     case 'EDIT_TASK':
-      return state.map((task) => {
-        if (task.id === action.id) {
-          return {
-            ...task,
-            ...action.updates,
-          }
-        } else {
-          return task
-        }
-      });
+      return editTask(state, action)
     default:
       return state;
   }
