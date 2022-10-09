4907dbddbdf9ad8eb16b413652a2f1d3a11a4cb5
Tyler Martin <tyler.martin@electric-playground.com>
2022-10-09 19:56:59 -0300

    refactor/v5-state-as-object updates task reducers and updates state as an object instead of an array

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,63 +1,59 @@
 let normalizedState = {
-  byId: [
-		{
+  byId: {
+		'task1': {
 			id: 'task1',
 			parentId: null,
 			subtasks: ['task1a'],
       content: 'todo item 1',
 		},
-		{
+		'task2': {
 			id: 'task2',
 			parentId: null,
 			subtasks: [],
       content: 'todo item 2',
 		},
-		{
+		'task1a': {
 			id: 'task1a',
 			parentId: 'task1',
 			subtasks: [],
       content: 'todo item 1a',
 		}
-	],
+},
 	allIds: ['task1', 'task2', 'task1a']
 }
 
 function editTask(state, action) {
   return {
-    byId: state.byId.map(task => {
-      if (task.id === action.id) {
-        return {...task, ...action.updates}
-      }
-      return task
-    }),
+    byId: {...state.byId, [action.id]: {...state.byId[action.id], ...action.updates}},
     allIds: state.allIds
   }
 }
 
 function addTask(state, action) {
-  let byId = state.byId.map(task => {
-    if (task.id === action.task.parentId) {
-      return {...task, subtasks: [...task.subtasks, action.task.id]}
-    }
-    return task
-  }) 
-  return {
-    byId: [...byId, action.task],
+  let byId = null
+  if (action.task.parentId) {
+    let parentTask = state.byId[action.task.parentId]
+    parentTask.subtasks = [...parentTask.subtasks, action.task.id]
+    byId = {...state.byId, [action.task.parentId]: parentTask, [action.task.id]: action.task} 
+  }
+
+  return { 
+    byId: byId || {...state.byId, [action.task.id]: action.task},
     allIds: [...state.allIds, action.task.id]
   }
 }
 
 function removeTask(state, action) {
-    let byIds = state.byId.map(task => {
-      if (task.id === action.task.parentId) {
-        return {...task, subtasks: task.subtasks.filter(id => id !== action.task.id)}
-      }
-      return task
-    })
-    return {
-      byId: byIds.filter(task => task.id !== action.task.id),
-      allIds: state.allIds.filter(id => !id === action.task.id)
-    }
+    // let byIds = state.byId.map(task => {
+    //   if (task.id === action.task.parentId) {
+    //     return {...task, subtasks: task.subtasks.filter(id => id !== action.task.id)}
+    //   }
+    //   return task
+    // })
+    // return {
+    //   byId: byIds.filter(task => task.id !== action.task.id),
+    //   allIds: state.allIds.filter(id => !id === action.task.id)
+    // }
 }
 
 export default (state = normalizedState, action) => {
